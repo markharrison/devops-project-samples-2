1cdfa077ccae2957eb096a1a85e3ae6f223cab9a
Mark Harrison <mark@markharrison.org>
2019-05-10 19:01:31 +0100

    Update WebApp .NET Core 2.2

--- devops-project-samples/dotnet/aspnetcore22/containerWithTests/Application/aspnet-core-dotnet-core.FunctionalTests/SampleFunctionalTests.cs
+++ devops-project-samples/dotnet/aspnetcore22/containerWithTests/Application/aspnet-core-dotnet-core.FunctionalTests/SampleFunctionalTests.cs
@@ -25,7 +25,7 @@ namespace SampleWebApplication.FunctionalTests
                 driver = GetChromeDriver();
                 var webAppUrl = testContext.Properties["webAppUrl"].ToString();
                 driver.Navigate().GoToUrl(webAppUrl);
-                Assert.AreEqual("Home Page - ASP.NET Core", driver.Title, "Expected title to be 'Home Page - ASP.NET Core'");
+                Assert.AreEqual("Home Page - ASP.NET Core 2.2", driver.Title, "Expected title to be 'Home Page - ASP.NET Core 2.2'");
             }
             finally
             {
